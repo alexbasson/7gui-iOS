6ab1525b40b0a5d0f0ea2d95c6b5f0f5656932ad
Alex Basson <abasson@vmware.com>
2024-08-12 13:23:12 -0400

    Initial Commit

---
+++ iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Alex Basson on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
